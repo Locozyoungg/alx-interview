39fadd0ac2388de1389b8226e5de9ff62f3d6c0f
Locozyoungg <collaustine27@gmail.com>
2024-08-09 02:03:15 +0300

    star

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -8,33 +8,31 @@ const movieId = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
 // Make the HTTP GET request to the Star Wars API
-request(url, function (error, response, body) {
+request(url, async function (error, response, body) {
   if (error) {
-    // Log the error if the request fails
     console.error('Error:', error);
   } else if (response.statusCode !== 200) {
-    // Log the status code if the request is not successful
     console.error('Failed to get data. Status code:', response.statusCode);
   } else {
-    // Parse the JSON response to get the movie data
     const data = JSON.parse(body);
     const characters = data.characters;
 
-    // Iterate over the list of characters and make a request for each one
-    characters.forEach(characterUrl => {
-      request(characterUrl, function (charError, charResponse, charBody) {
-        if (charError) {
-          // Log the error if the request fails
-          console.error('Error:', charError);
-        } else if (charResponse.statusCode !== 200) {
-          // Log the status code if the request is not successful
-          console.error('Failed to get character data. Status code:', charResponse.statusCode);
-        } else {
-          // Parse the character data and print the character's name
-          const characterData = JSON.parse(charBody);
-          console.log(characterData.name);
-        }
+    for (const characterUrl of characters) {
+      await new Promise((resolve, reject) => {
+        request(characterUrl, function (charError, charResponse, charBody) {
+          if (charError) {
+            console.error('Error:', charError);
+            reject(charError);
+          } else if (charResponse.statusCode !== 200) {
+            console.error('Failed to get character data. Status code:', charResponse.statusCode);
+            reject(new Error('Failed to get character data'));
+          } else {
+            const characterData = JSON.parse(charBody);
+            console.log(characterData.name);
+            resolve();
+          }
+        });
       });
-    });
+    }
   }
 });
