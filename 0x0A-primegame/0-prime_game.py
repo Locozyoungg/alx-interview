17e615a476101e549c915fd40615f04ee6de553c
Locozyoungg <collaustine27@gmail.com>
2024-09-05 15:21:04 +0300

    prime_game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,48 +1,35 @@
+
 #!/usr/bin/python3
-def sieve_of_eratosthenes(max_n):
-    """Generates a list of booleans indicating prime status for numbers up to max_n."""
-    primes = [True] * (max_n + 1)
-    primes[0] = primes[1] = False  # 0 and 1 are not primes
-    p = 2
-    while p * p <= max_n:
-        if primes[p]:
-            for i in range(p * p, max_n + 1, p):
-                primes[i] = False
-        p += 1
-    return primes
+""" Prime Game """
 
-def count_primes(n, primes):
-    """Returns the number of primes up to n using a precomputed list."""
-    return sum(primes[:n + 1])
 
-def isWinner(x, nums):
-    """Determines the winner of the prime game."""
-    if not nums or x < 1:
+def generatePrimeNumbers(limit):
+    """Generate list of primes up to limit"""
+    primeNumbers = []
+    sieveList = [True] * (limit + 1)
+    for potentialPrime in range(2, limit + 1):
+        if sieveList[potentialPrime]:
+            primeNumbers.append(potentialPrime)
+            for multiple in range(potentialPrime, limit + 1, potentialPrime):
+                sieveList[multiple] = False
+    return primeNumbers
+
+
+def isWinner(numRounds, roundValues):
+    """
+    Determine the winner of the game
+    """
+    if not numRounds or not roundValues:
         return None
-
-    max_n = max(nums)
-    primes = sieve_of_eratosthenes(max_n)
-    
-    maria_wins = 0
-    ben_wins = 0
-    
-    # Simulate each round
-    for n in nums:
-        if n == 1:
-            ben_wins += 1
-            continue
-
-        prime_count = count_primes(n, primes)
-        
-        # Maria wins if the count of primes is odd, Ben wins if it's even
-        if prime_count % 2 == 1:
-            maria_wins += 1
+    mariaScore = benScore = 0
+    for i in range(numRounds):
+        primes = generatePrimeNumbers(roundValues[i])
+        if len(primes) % 2 == 0:
+            benScore += 1
         else:
-            ben_wins += 1
-
-    if maria_wins > ben_wins:
+            mariaScore += 1
+    if mariaScore > benScore:
         return "Maria"
-    elif ben_wins > maria_wins:
+    elif benScore > mariaScore:
         return "Ben"
-    else:
-        return None
+    return None
